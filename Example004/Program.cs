966a95025cac084186d093a142859b46c79adad3
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-10 10:57:14 +0300

    changes in example004 (random numbers)

--- Example004/Program.cs
+++ Example004/Program.cs
@@ -1,3 +1,5 @@
-﻿double numA = 12;
-double numB = 5;
+﻿double numA = new Random().Next(1, 10);
+Console.WriteLine(numA);
+double numB = new Random().Next(1, 10);
+Console.WriteLine(numB);
 Console.WriteLine(numA/numB);
