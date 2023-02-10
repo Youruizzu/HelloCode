c1960964a5acce004df8704a9ddd30fca2cb1f04
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-10 10:37:44 +0300

    added .gitignore and fixed the code

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,3 +1,9 @@
-﻿console.Write("Введите свой никнейм: ");
-string username = Console.ReadLine();
-console.WriteLine("Привет, " + username);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите свой никнейм: ");
+        string username = Console.ReadLine();
+        Console.Write("Привет, " + username);
+    }
+}
