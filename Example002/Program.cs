676a7f1f3da6fa668712aa01fd4a85a0a3f6d8df
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-12 12:29:17 +0300

    fixed ex002 & ex005

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -2,8 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        Console.Write("Введите свой никнейм: ");
-        string username = Console.ReadLine();
-        Console.Write("Привет, " + username);
+        while (true) {
+            Console.Write("Введите свой никнейм: ");
+            string? userInputErrorTest = Console.ReadLine();
+            string username = userInputErrorTest ?? "NullErrorException";
+            if (username == "NullErrorException") {
+                Console.WriteLine("Error. Try again.");
+            }
+            else { Console.WriteLine("Привет, " + username); return;}
+        }
     }
 }
--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,9 +1,23 @@
-﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
-
-if(username.ToLower() == "маша") {
-    Console.WriteLine("Ура, это Маша!");
-} 
-else { 
-    Console.WriteLine("Привет, " + username);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true) {
+            Console.Write("Введите имя пользователя: ");
+            string? userInputErrorTest = Console.ReadLine();
+            string username = userInputErrorTest ?? "NullErrorException";
+            if (username == "NullErrorException") {
+                Console.WriteLine("Error. Try again.");
+            }
+            else {
+                if(username.ToLower() == "маша") {
+                    Console.WriteLine("Ура, это Маша!");
+                } 
+                else { 
+                    Console.WriteLine("Привет, " + username);
+                }
+                return;
+            }
+        }
+    }
 }
