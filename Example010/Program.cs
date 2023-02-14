06a621e7561e26c65483eeabb5c02fc12bb38230
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-14 23:10:52 +0300

    Added Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 112,211,13,4,15,16,17,181,91 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index<n){
+    if(array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
